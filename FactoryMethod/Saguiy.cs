b45030fbefb1a70dc7b2fe757ebc28f05a645bd8
rudida247 <rudida247@gmail.com>
2019-07-14 14:22:00 +0300

    ex2

---
+++ FactoryMethod/Saguiy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Saguiy : Vehicle
+    {
+        protected string rentalLocation;
+
+        public Saguiy(string model, float cylinder, string color, int numberOfWheels, string rentalLocation) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.rentalLocation = rentalLocation;
+        }
+
+        public override string ToString()
+        {
+            return "Saguiy " + base.ToString() + $" {rentalLocation}";
+        }
+    }
+}
---
+++ FactoryMethod/Truck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Truck : Vehicle
+    {
+        protected string truckUsage;
+
+        public Truck(string model, float cylinder, string color, int numberOfWheels, string truckUsage) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.truckUsage = truckUsage;
+        }
+
+        public override string ToString()
+        {
+            return "Truck " + base.ToString() + $" {truckUsage}";
+        }
+    }
+}
